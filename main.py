0df923922eacc7471cdeaf9a33eb376f9e41ad56
sammaelcz <jakubkrocil@gmail.com>
2025-01-10 20:50:18 +0100

    Kuba prvni commit

--- main.py
+++ main.py
@@ -26,7 +26,48 @@ def main():
         Predmet("Pálava", 200, 500)
     ])
 
-    osoba = Osoba(100, hradcany.jmeno)
+    #Začátek - vstupy:
+    osoba = Osoba(100, hradcany)
+
+    #Hlavní smyčka:
+    aktualni_den = 1
+    konec_hry = 15
+
+    while aktualni_den < konec_hry:
+        print(f"Je den {aktualni_den}.")
+        print(f"V portmonce máš: {osoba.hotovost},- Kč")
+        print(f"Nacházíš se v lokaci: {osoba.aktualni_lokace.jmeno}")
+        print("Dostupné lokace:")
+        print("1 - Hradčany")
+        print("2 - Václavák")
+        print("3 - Holešovice")
+        volba = input("Zadej číslo lokace: ")
+        if volba == 1:
+            osoba.zmen_lokaci(hradcany)
+        elif volba == 2:
+            osoba.zmen_lokaci(vaclavak)
+        elif volba == 3:
+            osoba.zmen_lokaci(holesovice)
+        else:
+            print("Neplatná volba")
+            continue
+   #     Co chceš dělat?
+    #    Přesun lokace
+        input()
+        aktualni_den += 1
+
+    print(f"Konec hry. Máš {osoba.hotovost}, jsi hotový boháč!")
+main()
+
+
+
+
+
+
+
+
+
+
 '''
     print()
     print(vaclavak.predmety[0].aktualni_cena)
@@ -37,6 +78,3 @@ def main():
 
     print(osoba.aktualni_lokace)
 '''
-
-main()
-
--- osoba.py
+++ osoba.py
@@ -1,9 +1,15 @@
+from asyncio import all_tasks
+
+
 class Osoba:
     def __init__(self, hotovost, aktualni_lokace):
         self.hotovost = hotovost
         self.aktualni_lokace = aktualni_lokace
         self.kupene_predmety = {}
 
-
+    def zmen_lokaci(self, nova_lokace):
+        print(f"Nacházíš se v: {self.aktualni_lokace.jmeno}")
+        self.aktualni_lokace = nova_lokace
+        print(f"Přesunul si se do: {self.aktualni_lokace.jmeno}")
 
 
